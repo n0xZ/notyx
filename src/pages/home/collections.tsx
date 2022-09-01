be48da84162f1e7c468e6ffaece5d2da8cf34aa5
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-01 17:34:29 -0300

    set new desktop layout on collections dynamic page

--- src/pages/home/collections.tsx
+++ src/pages/home/collections.tsx
@@ -1,10 +1,65 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet, useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { useUserId } from '@nhost/react'
+import { GET_COLLECTIONS_BY_USER_ID_QUERY } from '@/graphql/queries'
+import { Collection, CollectionQuery, CollectionQueryVariables } from '@/types'
+import { Icon } from '@iconify/react'
 
-export default function ColllectionOutlet() {
+type CollectionListProps = {
+	collections: Collection[]
+}
+function CollectionsListDisplay({ collections }: CollectionListProps) {
 	return (
-		<section className='xl:flex hidden'>
-			<Outlet />
-		</section>
+		<div className="grid grid-rows-3 place-items-center mt-6">
+			{collections.map((collection) => (
+				<div key={collection.collectionId}>
+					<NavLink
+						to={`/home/collections/${collection.collectionId}`}
+						className={({ isActive }) =>
+							`text-lg px-5 py-3  rounded-lg flex flex-row space-x-3 items-center w-64 mb-2 ${
+								isActive
+									? 'bg-rose-600 hover:bg-rose-800 text-neutral-50 '
+									: 'hover:bg-rose-600 hover:text-neutral-50 '
+							}  duration-100 ease-in-out`
+						}
+					>
+						<Icon icon="uil:folder" className="h-4 w-4" />
+						<span className='truncate'> {collection.title}</span>
+					</NavLink>
+				</div>
+			))}
+		</div>
+	)
+}
+export default function ColllectionOutlet() {
+	const params = useParams()
+	const userId = useUserId()
+	const {
+		data: collections,
+		loading,
+		error,
+	} = useQuery<CollectionQuery, CollectionQueryVariables>(
+		GET_COLLECTIONS_BY_USER_ID_QUERY,
+		{
+			variables: { userId },
+		}
+	)
+	return (
+		<>
+			<section className="xl:flex xl:flex-row h-screen  hidden">
+				<div className="w-96  shadow-sm   border-neutral-100 border-r-2 rounded h-full col-start-1 col-end-1 container mx-auto">
+					<h1 className=" font-bold text-lg mx-4 mt-2  ">COLECCIONES</h1>
+					{!loading && collections && (
+						<CollectionsListDisplay collections={collections?.collections} />
+					)}
+				</div>
+				<article className="grid place-items-center w-full items-center">
+					<Outlet />
+				</article>
+			</section>
+			<section className="xl:hidden lg:hidden sm:hidden  relative min-h-screen h-full">
+				<Outlet />
+			</section>
+		</>
 	)
 }
--- src/pages/home/collections/[collectionId].tsx
+++ src/pages/home/collections/[collectionId].tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import { Icon } from '@iconify/react'
+import { ChangeEvent, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Icon } from '@iconify/react'
 import { useQuery } from '@apollo/client'
 
 import { GET_COLLECTIONS_BY_COLLECTION_ID_QUERY } from '@/graphql/queries'
@@ -35,9 +35,11 @@ export default function CollectionById() {
 	}
 
 	return (
-		<section className="h-full space-y-2 ">
+		<section className="h-full space-y-2 container mx-auto">
 			<h1 className="text-3xl text-center font-semibold mt-2 mb-3">
-				Notas de mi colección
+				{loading
+					? 'Cargando notas de mi coleccion...'
+					: notes?.collections[0].title}
 			</h1>
 			<article className="h-12  mt-5  items-center space-x-3 mb-6 container mx-auto max-w-5xl flex flex-row justify-center">
 				<input
@@ -46,15 +48,21 @@ export default function CollectionById() {
 					value={noteName}
 					onChange={onChange}
 					name="noteName"
-					className="px-5 py-3  rounded-lg border-2 border-gray-200 xl:w-2/4 max-w-2xl"
+					className="px-5 py-3  max-w-xl rounded-lg border-2 border-gray-200 xl:w-2/4 "
 				/>
-				<button title="Crear nueva tarea">
-					<Icon icon="gg:add-r" className="h-8 w-8" onClick={openModal} />
+				<button title="Crear nueva nota">
+					<Icon icon="uil:envelope-add" className="h-8 w-8" onClick={openModal} />
 				</button>
 			</article>
 
 			{loading && <LoadingSkeleton />}
-			{notes && <NotesList notes={notes?.collections[0].collectionNotes} />}
+			{notes && notes.collections[0].collectionNotes.length !== 0 ? (
+				<NotesList notes={notes?.collections[0].collectionNotes} />
+			) : (
+				<p className="text-center mt-12">
+					No tienes notas creadas en esta colección por el momento.
+				</p>
+			)}
 
 			{/* <CreateNoteModal closeModal={closeModal} isOpen={isOpen} /> */}
 			<Toaster position="top-right" />
