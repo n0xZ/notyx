a1b45c422411c638e189883a8425e32344276e06
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-01 10:20:57 -0300

    remove file-base route from vite-plugin-pages and set Routes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import { AnimatePresence } from 'framer-motion'
-import { Suspense } from 'react'
-import { useLocation, useRoutes } from 'react-router-dom'
-import routes from '~react-pages'
+import { Suspense, lazy } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+
 import GlobalLoading from './components/loading/GlobalLoading'
+
+const Landing = lazy(() => import('./pages/index'))
+const Login = lazy(() => import('./pages/login'))
+const Register = lazy(() => import('./pages/register'))
+const HomeOutlet = lazy(() => import('./pages/home'))
+const HomeGeneral = lazy(() => import('./pages/home/general'))
+const Profile = lazy(() => import('./pages/home/profile'))
+const CollectionsOutlet = lazy(() => import('./pages/home/collections'))
+const DynamicCollections = lazy(
+	() => import('./pages/home/collections/[collectionId]')
+)
 
 function App() {
 	const location = useLocation()
+
 	return (
 		<AnimatePresence mode="wait" key={location.pathname}>
-			<Suspense fallback={<GlobalLoading />}>{useRoutes(routes)}</Suspense>
+			<Suspense fallback={<GlobalLoading />}>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/home" element={<HomeOutlet />}>
+						<Route path="general" element={<HomeGeneral />} />
+
+						<Route path="profile" element={<Profile />} />
+						<Route path="collections" element={<CollectionsOutlet />}>
+							<Route path=":collectionId" element={<DynamicCollections />} />
+						</Route>
+					</Route>
+				</Routes>
+			</Suspense>
 		</AnimatePresence>
 	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import Pages from 'vite-plugin-pages'
+
 import React from '@vitejs/plugin-react'
 
 import * as path from 'path'
 export default defineConfig({
-	plugins: [React(), Pages()],
+	plugins: [React()],
 	resolve: {
 		alias: {
 			'@': path.resolve('src'),
