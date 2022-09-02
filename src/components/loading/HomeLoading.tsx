4b6dff00b5bd4cd98706637614adfc919efbd1a7
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-02 15:22:59 -0300

    set home icon on home loading

--- src/components/loading/HomeLoading.tsx
+++ src/components/loading/HomeLoading.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import LoadingAvatar from './LoadingAvatar'
+import { Icon } from '@iconify/react'
 
 const transitionVariants = {
 	initial: {
@@ -21,8 +22,11 @@ export default function HomeLoading() {
 		<>
 			<header className="p-3 bg-rose-100 sticky top-0 h-19 w-full">
 				<nav className="flex flex-row items-center justify-between container mx-auto max-w-6xl">
-					<NavLink to="/home/main" className="h-6 w-6">
-						Home
+					<NavLink
+						to="/home/general"
+						className="font-bold xl:text-xl text-lg flex flex-row items-center space-x-2 hover:c-rose-400 duration-100 ease-in-out"
+					>
+						<Icon icon="uil:home-alt" className="h-8 w-8" />
 					</NavLink>
 					<LoadingAvatar />
 				</nav>
